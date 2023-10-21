b8236f265662443a47b02db9510ab39a379a7d8d
abhirathore04 <abhishekrathore1908@gmail.com>
2023-10-21 14:06:28 +0530

    sign in page and its api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import {signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post("/signup", signup);
-// router.post("/signin", signin);
-// router.post('/google', google);
-// router.get('/signout', signOut)
+router.post("/signin", signin);
 
 export default router;
