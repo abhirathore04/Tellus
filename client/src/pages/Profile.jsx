3297d957ea473bfca6f6a78ed9a296f7ed456abd
abhirathore04 <abhishekrathore1908@gmail.com>
2023-10-24 19:33:41 +0530

    complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -234,8 +234,7 @@ export default function Profile() {
         {showListingsError ? 'Error showing listings' : ''}
       </p>
 
-      {userListings &&
-        userListings.length > 0 &&(
+      {userListings && userListings.length > 0 &&(
         <div className="flex flex-col gap-4">
           <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
           {userListings.map((listing) => (
@@ -264,8 +263,9 @@ export default function Profile() {
                 >
                   Delete
                 </button>
-                
-                <button className='text-green-700 uppercase'>Edit</button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
               </div>
             </div>
           ))}
